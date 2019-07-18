443094313fe5f7e31a070efe23e89466c268f990
ZhangYang <33109790+jinsha741@users.noreply.github.com>
2019-07-18 10:57:43 +0900

    Create AngularJS模板.js

---
+++ AngularJS模板.js
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+
+<html>
+   <head>
+      <meta charset="utf-8">
+      <script src="http://code.angularjs.org/1.7.8/angular.min.js"></script>
+      <title>AngularJS 基本概念总结</title>
+   </head>
+
+   <body>
+
+      <h2>AngularJS body name</h2>
+
+      <script>
+
+      </script>
+      
+   </body>
+
+</html>
