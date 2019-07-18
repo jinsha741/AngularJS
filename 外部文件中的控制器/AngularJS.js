056fd573f7f69a24dd6b9490999b29b2cb886698
ZhangYang <33109790+jinsha741@users.noreply.github.com>
2019-07-18 11:03:29 +0900

    Update AngularJS.js

--- 外部文件中的控制器/AngularJS.js
+++ 外部文件中的控制器/AngularJS.js
@@ -1,11 +1,27 @@
-<div ng-app="myApp" ng-controller="namesCtrl">
+<!DOCTYPE html>
 
-<ul>
-  <li ng-repeat="x in names">
-    {{ x.name + ', ' + x.country }}
-  </li>
-</ul>
+<html>
+   <head>
+      <meta charset="utf-8">
+      <script src="http://code.angularjs.org/1.7.8/angular.min.js"></script>
+      <title>AngularJS 外部控制器调用方法</title>
+   </head>
 
-</div>
+   <body>
+      <h2>AngularJS 外部控制器调用方法</h2>
+      
+      <script>
+          <div ng-app="myApp" ng-controller="namesCtrl">
 
-<script src="namesController.js"></script>
+            <ul>
+              <li ng-repeat="x in names">
+                {{ x.name + ', ' + x.country }}
+              </li>
+            </ul>
+          </div>
+      </script>
+      <script src="namesController.js"></script>
+
+   </body>
+
+</html>
