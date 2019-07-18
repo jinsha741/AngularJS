cfb5eb0462f65d0f1511ab2459c87750236043a4
ZhangYang <33109790+jinsha741@users.noreply.github.com>
2019-07-18 13:30:58 +0900

    Update AngularJS.js

--- 依赖注入/AngularJS.js
+++ 依赖注入/AngularJS.js
@@ -11,7 +11,7 @@
 
       <h2>AngularJS 依赖注入</h2>
 
-	    // 静态页面实现
+	// 静态页面实现
       <div ng-app = "mainApp" ng-controller = "CalcController">
          <p>输入一个数字: <input type = "number" ng-model = "number" /></p>
          <button ng-click = "square()">X<sup>2</sup></button>
@@ -21,8 +21,8 @@
       <script>
          var mainApp = angular.module("mainApp", []);       //通过angular.module函数来创建模块
          
-		     //【provider】通过 provider 创建一个 service、factory等(配置阶段)
-		     // Provider 中提供了一个 factory 方法 get()，它用于返回 value/service/factory
+	//【provider】通过 provider 创建一个 service、factory等(配置阶段)
+	// Provider 中提供了一个 factory 方法 get()，它用于返回 value/service/factory
          mainApp.config(function($provide) {
             $provide.provider('MathService', function() {
                this.$get = function() {
@@ -36,10 +36,10 @@
             });
          });
 		
-		     //【value】Value是一个简单的javascript对象，用于向控制器传递值（初始化配置阶段）
+     	//【value】Value是一个简单的javascript对象，用于向控制器传递值（初始化配置阶段）
          mainApp.value("defaultInput", 5);
          
-		     //【factory】创建 factory "MathService" 用于两数的乘积
+     	//【factory】创建 factory "MathService" 用于两数的乘积
          mainApp.factory('MathService', function() {
             var factory = {};
             
@@ -49,7 +49,7 @@
             return factory;
          });
          
-		     //【factory】在 service 中注入 factory "MathService"(依赖注入过程)
+     	//【factory】在 service 中注入 factory "MathService"(依赖注入过程)
          mainApp.service('CalcService', function(MathService){
             this.square = function(a) {
                return MathService.multiply(a,a);
