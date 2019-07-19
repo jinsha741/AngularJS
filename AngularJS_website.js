c7d95df9792579391706f21d4763843d4ae0a364
ZhangYang <33109790+jinsha741@users.noreply.github.com>
2019-07-19 13:52:48 +0900

    Create AngularJS_website.js

---
+++ AngularJS_website.js
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+
+<html>
+   <head>
+      <meta charset="utf-8">
+      <script src="http://code.angularjs.org/1.7.8/angular.min.js"></script>
+      <title>AngularJS 解说网站</title>
+   </head>
+
+   <body>
+
+      <h2>AngularJS 解说网站</h2>
+      <p>
+        <ul>
+        
+          <li>
+            <a href = 'https://www.oschina.net/translate/angularjs-factory-vs-service-vs-provider?lang=chs&page=1' target="_blank">
+              AngularJS 之 Factory vs Service vs Provider
+            </a>
+          </li>
+          <li>
+            <a href = 'https://blog.csdn.net/flyingpig2016/article/details/71632414' target="_blank">
+              对AngularJs中依赖注入的理解和使用方法
+            </a>
+          </li>
+          
+          <li>
+            <a href = 'https://segmentfault.com/a/1190000003096933' target="_blank">
+              AngularJS中的Provider们：Service和Factory等的区别
+            </a>
+          </li>
+        
+        <ul>
+      </p>
+      <script>
+
+      </script>
+      
+   </body>
+
+</html>
