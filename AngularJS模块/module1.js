c669791e0538f573a25a0d23f2940fac3ec37259
ZhangYang <33109790+jinsha741@users.noreply.github.com>
2019-07-19 17:24:08 +0900

    Update module1.js

--- AngularJS模块/module1.js
+++ AngularJS模块/module1.js
@@ -2,3 +2,12 @@
 var module1 = angular.module("module1" , []);
 var module2 = angular.module("module2" , []);
 
+angular.element(document).ready(function(){
+  // 获得指定id的html元素
+  var div1 = document.getElementById("div1");
+  var div2 = document.getElementById("div2");
+  
+  // 使用AngularJS的bootstrap方法给HTML元素指定模块
+  angular.bootstrap(div1 , ["module1"]);
+  angular.bootstrap(div2 , ["module2"]);
+});
