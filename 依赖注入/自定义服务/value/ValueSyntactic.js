c9b5f6cab10cef2b0e14afdf98b51f7463ae18c5
ZhangYang <33109790+jinsha741@users.noreply.github.com>
2019-07-21 17:16:05 +0900

    Update ValueSyntactic.js

--- 依赖注入/自定义服务/value/ValueSyntactic.js
+++ 依赖注入/自定义服务/value/ValueSyntactic.js
@@ -1,5 +1,12 @@
 var appModule = angular.module("appModule" , []);
+
 // 真正完整的写法是在模块的.config()方法中通过$provide服务去创建
 appModule.config(function($provide){
     $provide.value("name" , "ZHANGYANG");
 });
+
+// 创建好服务后通过AngularJS的注入机制将其注入到Controller中
+// $scope服务的其中一个作用就是给Controller添加属性和方法
+appModule.controller("appController" , ["$scope" , "name" , function($scope , name){
+    $scope.name = name;
+}]);
