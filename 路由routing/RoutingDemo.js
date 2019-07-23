b503eff9bda2a08bca198932f3ec0a70a7618c9a
ZhangYang <33109790+jinsha741@users.noreply.github.com>
2019-07-23 11:44:00 +0900

    Update RoutingDemo.js

--- 路由routing/RoutingDemo.js
+++ 路由routing/RoutingDemo.js
@@ -1,5 +1,8 @@
+// 为了给应用配置路由，需要给应用创建一个模块。这个模块叫做appModule
 var appModule = angular.module("appModule" , ["ngRoute"]);
 
+// 配置 $routeProvider，AngularJS $routeProvider 用来定义路由规则
+// 并且通过使用configAPI请求把$routeProvider注入到配置函数并且使用$routeProvider.whenAPI来定义路由规则
 appModule.config(["$routeProvider" , function($routeProvider){
   $routeProvider
     .when("/" , {template : "此处应该有首页页面"})
